8bcf69cc86b0ed4ef3fb3bd51146e70e477aa00e
Welington Machado <welington.machado.2001@gmail.com>
2020-08-03 20:12:45 -0300

    modificando

--- main.cpp
+++ main.cpp
@@ -6,15 +6,12 @@ class celula
 {
 private:
 	string nome;
-	celula *proxima;//*refere-se ao ponteiro , salvando endereço da próxima celula.
-	
-public:
-	celula()//celula() é um metodo
-	{
+	celula * proxima;	
+public:	
+	celula(){
 		proxima = NULL;
 	}
-	celula(string n)
-	{
+	celula(string n){
 		nome = n;
 	}
 	string getNome(){
@@ -23,14 +20,16 @@ public:
 	void setNome(string n){
 		nome = n;
 	}
+	
 	celula * getProxima(){
 		return proxima;
 	}
-	/*~celula(){
-		cout << "Passou no metodo destrutor";
-	}*/
 	
+	void setProxima(celula * l){
+		proxima = l;
+	}
 };
+
 
 class lista
 {
@@ -44,48 +43,51 @@ public:
 	celula * getInicio(){
 		return inicio;
 	}
-	void inserirInicio(string n)
-	{
+	
+	void inserirInicio(string n){
 		celula *nova = new celula;
 		nova->setNome(n);
+		nova->setProxima(inicio);
 		inicio = nova;
 	}
-	void inserirFim (string n){
+	
+	void inserirFim(string n){
 		celula *nova = new celula(n);
-		nova->getProxima(NULL);
-		if(inicio != NULL){
+		nova->setProxima(NULL);
+		if(inicio != NULL)
+		{			
 			celula *aux = inicio;
-			while(aux->getProxima()!=NULL)
+			while(aux->getProxima() != NULL)
 			{
-				cout << aux->getProxima() << endl;
 				aux = aux->getProxima();
 			}
+			aux->setProxima(nova);
 		}
-		
+		else{
+			inicio = nova;
+		}
 	}
+	
 	void imprimir(){
 		if(inicio == NULL){
 			cout << "Lista vazia.\n";
-		}	else{
+		}
+		else{
 			celula *aux = inicio;
-			while(aux != NULL)
-			{
-				cout << aux->getNome() << "\n";
-				aux = aux->getProxima;
+			while(aux != NULL){
+				cout << aux->getNome() << endl;
+				aux = aux->getProxima();
 			}
 		}
 	}
-	
 };
 
-int main(int argc, char** argv)
-{
-	setlocale(LC_ALL, "portuguese");
+int main(){
 	lista listaAlunos;
-	listaAlunos.inserirInicio("Mailson");
-	listaAlunos.inserirInicio("João");
-	listaAlunos.inserirInicio("Maria");
+	listaAlunos.inserirFim("Joao");
+	listaAlunos.inserirFim("Maria");
+	listaAlunos.inserirFim("Jose");
+	listaAlunos.inserirInicio("Miguel");
 	listaAlunos.imprimir();
-	
 	return 0;
 }
